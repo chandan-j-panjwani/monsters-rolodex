dfacf2df7b9c2fa1720477c120a7731f07e7683f
Chandan Jagdish Panjwani <cpanjwani0000@gmail.com>
2020-09-16 18:24:39 +0530

    Updates

---
+++ precache-manifest.5447779d6f46eb328de4173b9be4f17d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52e915a7c73e9100d1908628de821c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c66a8ee6d34882f39c4a",
+    "url": "/static/css/main.4eb961d7.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66a8ee6d34882f39c4a",
+    "url": "/static/js/main.67738e26.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  }
+]);
